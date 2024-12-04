fcbffd7fde9358e90573c82ff7416e2a10ee20c9
latha <matthelatha443@gmail.com>
2024-12-04 14:33:25 +0530

    calculator

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./Header.css"
+
+
+
+function Header() {
+  return (
+    <div className='main-header'>
+        
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Study</li>
+        <li>Contact us</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Header;
